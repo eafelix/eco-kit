e7032783c2848e8a11da9cb43d1bfba30f26fa9a
Felix Enriquez <efelixagustin@gmail.com>
2020-10-05 16:35:53 -0300

    asd

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var app = express();
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static('public'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
